6408a3e42d9a40f48b4980ff5ea913b955a73a36
Juraj Kubala <juraj.alabuk@gmail.com>
2016-11-25 13:37:27 +0200

    bootstrap - employee list

--- app/customerX/react/components/employee-list/index.js
+++ app/customerX/react/components/employee-list/index.js
@@ -1,9 +1,5 @@
 import "./style.less";
 import React from "react";
-import {EmployeeModal} from '../employee-modal';
-
-const REF_EDIT_MODAL = 'detail';
-const REF_DETAIL_MODAL = 'detail';
 
 /**
  *
@@ -12,44 +8,93 @@ class EmployeeList extends React.Component {
 
     /**
      *
-     * @param props
+     * @param {MouseEvent} e
+     * @param employee
+     * @private
      */
-    constructor(props) {
-        super(props);
+    _onEditClick = (e, employee) => {
+        e.stopPropagation();
 
-        this._renderRow = this._renderRow.bind(this);
-    }
+        //noinspection JSUnresolvedVariable
+        if (typeof this.props.onEditClick == "function") {
 
+            //noinspection JSUnresolvedFunction
+            this.props.onEditClick(employee);
+        }
+
+    };
+
+    /**
+     *
+     * @param {MouseEvent} e
+     * @param employee
+     * @private
+     */
+    _onDeleteClick = (e, employee) => {
+        e.stopPropagation();
+
+        //noinspection JSUnresolvedVariable
+        if (typeof this.props.onDeleteClick == "function") {
+
+            //noinspection JSUnresolvedFunction
+            this.props.onDeleteClick(employee)
+        }
+    };
+
+    /**
+     *
+     * @param {MouseEvent} e
+     * @param employee
+     * @private
+     */
+    _onRowClick = (e, employee) => {
+
+        //noinspection JSUnresolvedVariable
+        if (typeof this.props.onRowClick == "function") {
+
+            //noinspection JSUnresolvedFunction
+            this.props.onRowClick(employee)
+        }
+    };
 
     /**
      *
      * @param employee
-     * @param key
+     * @param {number} key
      * @returns {XML}
      * @private
      */
-    _renderRow(employee, key) {
-        return <tr key={key} onClick={() => this.refs[REF_DETAIL_MODAL].showModal(employee)}>
+    _renderRow = (employee, key) => {
+        const salary = (1500 + Math.random() * 1000).toFixed(2);
+        return <tr key={key} onClick={(e) => this._onRowClick(e, employee)}>
             <td>{employee.name}</td>
-            <td>{Math.random() * 1500}E</td>
+            <td>{employee.email}</td>
+            <td>{employee.phone}</td>
+            <td style={{width: '150px', textAlign: 'right'}}>{salary} <span className="fa fa-eur"/></td>
+            <td className="actions-column">
+                <button className="btn btn-link btn-sm" onClick={(e) => this._onEditClick(e, employee)}>
+                    <i className="fa fa-pencil"/> Edit
+                </button>
+                <button className="btn btn-link btn-sm" onClick={(e) => this._onDeleteClick(e, employee)}>
+                    <i className="fa fa-trash-o"/> Delete
+                </button>
+            </td>
         </tr>
-    }
-
+    };
 
     /**
      *
      * @returns {XML}
      * @private
      */
-    _renderHead() {
-        return <thead>
-        <tr>
-            <th>Name</th>
-            <th>Sallary</th>
-        </tr>
-        </thead>;
-    }
+    _renderHead = () => {
 
+        const columns = ['Name', 'Email', 'Phone', 'Month salary', ''];
+
+        return <thead>
+        <tr>{columns.map((column, i) => <th key={i}>{column}</th>)}</tr>
+        </thead>;
+    };
 
     /**
      *
@@ -58,18 +103,16 @@ class EmployeeList extends React.Component {
     render() {
         return <div className="employee-list-wrapper">
 
-            <EmployeeModal title="Edit employee"/>
+            <table className="table table-hover table-striped">
 
-            <EmployeeModal title="Detail"
-                           ref={REF_DETAIL_MODAL}
-                           type="detail"/>
-
-            <table className="employee-list">
                 {this._renderHead()}
+
                 <tbody>
                 {this.props.data.map(this._renderRow)}
                 </tbody>
+
             </table>
+
         </div>
     }
 }
